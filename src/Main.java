b4284d9c6589485e454610b76e5faa0fe894c197
Сергей Душенбиев <agnium@mail.ru>
2022-02-02 17:07:41 +0300

    Java 2.1

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticketPrice = 2000;
+        int rublesPerMile = 20;
+        int totalMiles = ticketPrice / rublesPerMile;
+        System.out.println(totalMiles);
+    }
+}
